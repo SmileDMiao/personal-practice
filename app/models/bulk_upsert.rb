2cc5c38bfd211cf362c64618198a9d4a47d24b86
smiledmiao <m8023zsm@live.com>
2017-06-15 19:00:54 +0800

    upsert interface

---
+++ app/models/bulk_upsert.rb
@@ -0,0 +1,2 @@
+class BulkUpsert < ApplicationRecord
+end
---
+++ config/initializers/upsert.rb
@@ -0,0 +1,41 @@
+require 'active_record'
+
+module ActiveRecord
+  class Base
+    # fields = %w(name email city created_at updated_at)
+    # rows = [
+    #     ["smile", '2268571581@qq.com', 'shanghai', '2016-01-01', '2016-01-01'],
+    #     ['malzahar', 'm8023zsm@live.com', 'beijing', '2016-01-01', '2016-01-01'],
+    # ]
+    # BulkUpsert.bulk_write(fields, rows, :conflict => [:name])
+
+    def self.bulk_write(fields, rows, upsert = nil)
+      return 0 if rows.empty?
+      rows = rows.map do |row|
+        values = row.map.with_index do |val|
+          connection.quote(val)
+        end.join ', '
+        "(#{ values })"
+      end
+      field_list = fields.map {|e| %Q("#{e}")}.join ', '
+
+      sql = "INSERT INTO #{ table_name } (#{ field_list }) VALUES #{ rows.join ', ' }"
+      if upsert
+        if !upsert[:update]
+          update = fields.map(&:to_s) - upsert[:conflict].map(&:to_s)
+        else
+          update = upsert[:update]
+        end
+        update = update.map {|field| "#{ field } = EXCLUDED.#{ field }"}
+
+        sql += " ON CONFLICT (#{ upsert[:conflict].join ', ' }) DO UPDATE SET #{ update.join ', ' }"
+        if upsert[:where]
+          sql += " WHERE #{ upsert[:where] }"
+        end
+      end
+      res = connection.execute(sql)
+      res.cmd_tuples
+    end
+
+  end
+end
