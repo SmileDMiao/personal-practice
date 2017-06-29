329b16b764ca3250264a88bae4b6a7e847ec42b9
smiledmiao <m8023zsm@live.com>
2017-06-29 14:40:03 +0800

    mysql version upsert

--- config/initializers/upsert.rb
+++ config/initializers/upsert.rb
@@ -8,8 +8,10 @@ module ActiveRecord
     #     ['malzahar', 'm8023zsm@live.com', 'beijing', '2016-01-01', '2016-01-01'],
     # ]
     # BulkUpsert.bulk_write(fields, rows, :conflict => [:name])
+    # conflict: uniq index columns
+    # update: if conflict update columns
 
-    def self.bulk_write(fields, rows, upsert = nil)
+    def self.bulk_write_pg(fields, rows, upsert = nil)
       return 0 if rows.empty?
       rows = rows.map do |row|
         values = row.map.with_index do |val|
@@ -35,6 +37,40 @@ module ActiveRecord
       end
       res = connection.execute(sql)
       res.cmd_tuples
+    end
+
+
+    # fields = %w(name email city created_at updated_at)
+    # rows = [
+    #     ["smile", '2268571581@qq.com', 'shanghai', '2016-01-01', '2016-01-01'],
+    #     ['malzahar', 'm8023zsm@live.com', 'beijing', '2016-01-01', '2016-01-01'],
+    # ]
+
+    def self.bulk_write_mysql(fields, rows, upsert = nil)
+      return 0 if rows.empty?
+      rows = rows.map do |row|
+        values = row.map.with_index do |val|
+          connection.quote(val)
+        end.join ', '
+        "(#{ values })"
+      end
+      field_list = fields.join ', '
+
+      sql = "INSERT INTO #{ table_name } (#{ field_list }) VALUES #{ rows.join ', ' }"
+      if upsert
+        if !upsert[:update]
+          update = fields.map(&:to_s) - upsert[:conflict].map(&:to_s)
+        else
+          update = upsert[:update]
+        end
+        update = update.map {|field| "#{ field } = VALUES(#{ field })"}
+
+        sql += " ON DUPLICATE KEY UPDATE #{ update.join ', ' }"
+        if upsert[:where]
+          sql += " WHERE #{ upsert[:where] }"
+        end
+      end
+      connection.execute(sql)
     end
 
   end
