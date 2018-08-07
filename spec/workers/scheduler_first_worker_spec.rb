4b7850673938f28ae2babbaf04aaaaf5468c9ac7
Qingxin Miao <qingxin.miao@autodesk.com>
2018-08-07 18:37:49 +0800

    Sidekiq Test

---
+++ spec/workers/scheduler_first_worker_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
+
+RSpec.describe SchedulerFirstWorker, type: :worker do
+
+  describe ".perform" do
+    it "should work" do
+      SchedulerFirstWorker.perform_async
+      assert_equal 1, SchedulerFirstWorker.jobs.size
+      SchedulerFirstWorker.drain
+      assert_equal 0, SchedulerFirstWorker.jobs.size
+    end
+  end
+
+end
